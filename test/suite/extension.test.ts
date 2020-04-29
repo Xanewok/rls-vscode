33b6b6e74ad533dcb9da83b56b03f7e8c43471a4
Igor Matuszewski <xanewok@gmail.com>
2020-04-29 21:34:13 +0200

    Fix the task detection test

--- test/suite/extension.test.ts
+++ test/suite/extension.test.ts
@@ -17,14 +17,6 @@ suite('Extension Tests', () => {
       path.join(projectPath, 'another-lib-project'),
     ];
 
-    await vscode.commands.executeCommand('vscode.openFolder', projectUri);
-    await vscode.workspace.openTextDocument(
-      Uri.file(path.join(projects[0], 'src', 'lib.rs')),
-    );
-    await vscode.workspace.openTextDocument(
-      Uri.file(path.join(projects[1], 'src', 'lib.rs')),
-    );
-
     const expected = [
       { subcommand: 'build', group: vscode.TaskGroup.Build, cwd: projects[0] },
       { subcommand: 'build', group: vscode.TaskGroup.Build, cwd: projects[1] },
@@ -34,22 +26,56 @@ suite('Extension Tests', () => {
       { subcommand: 'run', group: undefined },
     ];
 
-    const tasks = await vscode.tasks.fetchTasks();
+    await vscode.commands.executeCommand('vscode.openFolder', projectUri);
 
-    for (const { subcommand, group, cwd } of expected) {
-      assert(
-        tasks.some(
-          task =>
-            task.definition.type === 'cargo' &&
-            task.definition.subcommand === subcommand &&
-            task.group === group &&
-            (!cwd ||
-              cwd ===
-                (task.execution &&
-                  task.execution.options &&
-                  task.execution.options.cwd)),
-        ),
-      );
-    }
+    // This makes sure that we set the focus on the opened files (which is what
+    // actually triggers the extension for the project)
+    await vscode.commands.executeCommand(
+      'workbench.action.quickOpen',
+      path.join(projects[0], 'src', 'lib.rs'),
+    );
+    await vscode.commands.executeCommand(
+      'workbench.action.acceptSelectedQuickOpenItem',
+    );
+    await vscode.commands.executeCommand('workbench.action.keepEditor');
+    // Unfortunately, we need to wait a bit for the extension to kick in :(
+    // FIXME: See if we can directly import our extension and await its progress
+    await new Promise(resolve => setTimeout(resolve, 500));
+    assert(await currentTasksInclude([expected[0]]));
+
+    // Now test for the second project
+    await vscode.commands.executeCommand(
+      'workbench.action.quickOpen',
+      path.join(projects[1], 'src', 'lib.rs'),
+    );
+    await vscode.commands.executeCommand(
+      'workbench.action.acceptSelectedQuickOpenItem',
+    );
+    await new Promise(resolve => setTimeout(resolve, 500));
+    assert(await currentTasksInclude(expected));
   }).timeout(0);
 });
+
+async function currentTasksInclude(
+  expected: Array<{
+    subcommand: string;
+    group: vscode.TaskGroup | undefined;
+    cwd?: string;
+  }>,
+): Promise<boolean> {
+  const tasks = await vscode.tasks.fetchTasks();
+
+  return expected.every(({ subcommand, group, cwd }) =>
+    tasks.some(
+      task =>
+        task.definition.type === 'cargo' &&
+        task.definition.subcommand === subcommand &&
+        task.group === group &&
+        (!cwd ||
+          cwd ===
+            (task.execution &&
+              task.execution.options &&
+              task.execution.options.cwd)),
+    ),
+  );
+}
